e8f41e5cc019552baa9b550063a92b946f699096
tingyun <tingyun@pinduoduo.com>
2020-09-01 10:21:52 +0800

    init

--- packages/admin/next-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-
-declare module "*.scss" {
-  const content: { [className: string]: string };
-  export default content;
-}
